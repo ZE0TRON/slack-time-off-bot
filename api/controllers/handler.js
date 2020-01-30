3227d7d494bb9bed57eb6bd17d935cdcfd90a4c4
ZE0TRON <cmpbilge@gmail.com>
2020-01-30 20:35:48 +0300

    project structure improved

--- api/controllers/handler.js
+++ api/controllers/handler.js
@@ -1,15 +1,14 @@
 const policyController = require("./policy");
 const timeOffController = require("./timeOff");
-const request = require("request");
-
 exports.handleCommand = (req, res, next) => {
   let text = req.body.text;
   let command = text.split(" ")[0];
   let responseUrl = req.body.response_url;
   let trigger_id = req.body.trigger_id;
+  let user_name = req.body.user_name;
   switch (command) {
     case "create_policy":
-      policyController.createPolicy();
+      policyController.createPolicy(trigger_id,user_name);
       break;
     case "delete_policy":
       policyController.deletePolicy();
@@ -25,81 +24,5 @@ exports.handleCommand = (req, res, next) => {
   }
   console.log(req.body);
   console.log(process.env.TOKEN);
-  request.post(
-    "https://slack.com/api/views.open",
-    {
-      headers: {
-        Authorization: "Bearer " + process.env.TOKEN
-      },
-      json: {
-        trigger_id: trigger_id,
-        view: {
-          type: "modal",
-          title: {
-            type: "plain_text",
-            text: "Create Policy",
-            emoji: true
-          },
-          submit: {
-            type: "plain_text",
-            text: "Create",
-            emoji: true
-          },
-          close: {
-            type: "plain_text",
-            text: "Cancel",
-            emoji: true
-          },
-          blocks: [
-            {
-              type: "input",
-              element: {
-                type: "plain_text_input",
-                action_id: "sl_input",
-                placeholder: {
-                  type: "plain_text",
-                  text: "Enter policy name"
-                }
-              },
-              label: {
-                type: "plain_text",
-                text: "Policy Name"
-              },
-              hint: {
-                type: "plain_text",
-                text: "Name of Your Time Off Policy"
-              }
-            },
-            {
-              type: "input",
-              element: {
-                type: "plain_text_input",
-                action_id: "ml_input",
-                placeholder: {
-                  type: "plain_text",
-                  text: "Enter maximum days"
-                }
-              },
-              label: {
-                type: "plain_text",
-                text: "Max Day"
-              },
-              hint: {
-                type: "plain_text",
-                text: "Enter maximum days allowed in the policy"
-              }
-            }
-          ]
-        }
-      }
-    },
-    (error, res, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-      console.log(`statusCode: ${res.statusCode}`);
-      console.log(body);
-    }
-  );
+  
 };
--- api/controllers/policy.js
+++ api/controllers/policy.js
@@ -1,5 +1,28 @@
-exports.createPolicy = () => {
+const createPolicyTemplate = require("../../build_kit_templates/createPolicy.json");
+const request = require("request");
+
+exports.createPolicy = (trigger_id,userName) => {
     console.log("Create Policy");
+    request.post(
+      "https://slack.com/api/views.open",
+      {
+        headers: {
+          Authorization: "Bearer " + process.env.TOKEN
+        },
+        json: {
+          trigger_id: trigger_id,
+          view: createPolicyTemplate
+        }
+      },
+      (error, res, body) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+        console.log(`statusCode: ${res.statusCode}`);
+        console.log(body);
+      }
+    );
 };
 exports.deletePolicy = () => {
     console.log("Delete Policy");
