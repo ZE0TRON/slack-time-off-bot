aa9c4420bcb1ec6dd1c500febe740d39b9cc6bdf
ZE0TRON <cmpbilge@gmail.com>
2020-01-30 16:12:09 +0300

    request lib added example response post sent

--- api/controllers/handler.js
+++ api/controllers/handler.js
@@ -1,8 +1,12 @@
 const policyController = require("./policy");
 const timeOffController = require("./timeOff");
+const request = require("request");
+
+
 exports.handleCommand = (req,res,next) => {
   let text = req.body.text;
   let command = text.split(" ")[0];
+  let responseUrl = req.body.response_url;
   switch(command) {
     case "create_policy":
       policyController.createPolicy();
@@ -17,7 +21,23 @@ exports.handleCommand = (req,res,next) => {
       timeOffController.cancelTimeOff();
       break;
     default:
-      res.send("Invalid Command");
+      return res.send("Invalid Command");
   }
   console.log(req.body);
+  request.post(
+   responseUrl,
+    {
+      json: {
+        text: "We will process it."
+      }
+    },
+    (error, res, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      console.log(`statusCode: ${res.statusCode}`);
+      console.log(body);
+    }
+  );
 };
