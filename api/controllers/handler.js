710413941b0e1c85ed3c60a8e8c4ccea4a9f35e1
ZE0TRON <cmpbilge@gmail.com>
2020-02-04 15:53:51 +0300

    max_day parse error handling updated

--- api/controllers/handler.js
+++ api/controllers/handler.js
@@ -67,7 +67,7 @@ exports.handlePayload = (req, res, next) => {
         // Parse values from state
         const policy_name = state.values.policy_name.sl_input.value;
         const max_days = parseInt(state.values.max_day.sl_input.value);
-        if (max_days == null) {
+        if (max_days == NaN) {
           const err = {
             msg: "Max day should be a number",
             block: "max_day"
