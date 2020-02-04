6057fc898e8bc28ffc3f13ab89d2c8fa6c9d3932
ZE0TRON <cmpbilge@gmail.com>
2020-02-04 16:15:00 +0300

    timeOff model and functions

---
+++ api/model/timeOff.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const timeOffSchema = new mongoose.Schema({
+  user_name: String,
+  policy_name: String,
+  date: String
+});
+
+// Gets all timeOffs of the user  from database
+timeOffSchema.statics.getTimeOffsByUser = function(userName) {
+  return new Promise((resolve, reject) => {
+    TimeOff.find({ user_name: userName }, (err, timeOffs) => {
+      if (err) {
+        reject(err);
+      }
+      if (timeOffs.length == 0) {
+        reject("You have no time off request");
+      }
+      resolve(timeOffs);
+    });
+  });
+};
+
+timeOffSchema.statics.getTimeOffs = function() {
+  return new Promise((resolve, reject) => {
+    TimeOff.find({}, (err, timeOffs) => {
+      if (err) {
+        reject(err);
+      }
+      if (timeOffs.length == 0) {
+        reject("There is no timeOff request");
+      }
+      resolve(timeOffs);
+    });
+  });
+};
+
+const TimeOff = mongoose.model("TimeOff", timeOffSchema);
+module.exports = TimeOff;
