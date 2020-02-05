c956e0a3ea62cdb00f839ee57ce6d045c97bd1a1
ZE0TRON <cmpbilge@gmail.com>
2020-02-05 10:33:26 +0300

    user_id null bug fix

--- api/controllers/handler.js
+++ api/controllers/handler.js
@@ -60,7 +60,7 @@ exports.handlePayload = (req, res, next) => {
     return res.send("Invalid Token");
   }
   console.log(req.body);
-  const user_id = req.body.user_id;
+  const user_id = req.body.user.id;
   const userName = payload.user.username;
   // If its a payload created with interacting with a slack view.
   if (payload.view != null) {
